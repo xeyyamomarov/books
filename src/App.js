f8b5949ac74663eedfbd30e8e20b57d5a2d98b4e
unknown <xeyyamomarov2014@gmail.com>
2023-01-29 17:09:41 +0400

    crud was added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { BookCreate } from "./components/BookCreate";
-import {BookList} from "./components/BooksList"
+import { BookList } from "./components/BooksList";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createaBooks = (title) => {
-    const updateBooks=[...books,{id:Math.round(Math.random()*9999),title}]
-    setBooks(updateBooks)
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
   };
 
-  const editBooks=(id,newTitle)=>{
-    const updateBooks = books.map((book)=>{
-      if(id===book.id){
-        return {...book,title:newTitle}
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const createaBooks = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    const updateBooks = [...books, response.data];
+    setBooks(updateBooks);
+  };
+
+  const editBooks = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+    console.log(response);
+    const updateBooks = books.map((book) => {
+      if (id === book.id) {
+        return { ...book, ...response.data };
       }
-      return book
-    })
-    setBooks(updateBooks)
-  }
+      return book;
+    });
+    setBooks(updateBooks);
+  };
 
-  const deleteBooks=(id)=>{
-    const updateBooks = books.filter(book=>{
-      return book.id !==id
-    })
-    setBooks(updateBooks)
-  }
+  const deleteBooks = async (id) => {
 
-
-
+    await axios.delete(`http://localhost:3001/books/${id}`)
+    const updateBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updateBooks);
+  };
 
   return (
     <div className="App">
